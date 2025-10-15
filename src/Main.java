3ac7a0c42a1c3f3d3433472731f6c5df89d8d978
andrey <andrey.sn41@gmail.com>
2025-10-15 17:01:20 +0500

    M2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Animal[] animals = {
+                new Cat("Бублик", 1000, "Мяу"),
+                new Dog("Колобок", 10, "Гав"),
+                new Bird("Кешка", 0.5f, "КЕША ПИРАТ"),
+                new Fish("Goldfish", 0.01f)
+        };
+        for( Animal animal:animals){
+            animal.eat(10);
+            if(animal instanceof Speaker){
+                ((Speaker) animal).say();
+            }
         }
+    }
+}
+
+class Animal {
+    private String name;
+    private float satisfactionLevel = 20;
+    private float satiety;
+
+    public Animal(String name, float satisfactionLevel) {
+        this.satisfactionLevel = satisfactionLevel;
+    }
+    float eat(float eat){
+         satiety +=  eat;
+         return satiety;
+    }
+}
+
+class Speaker extends Animal {
+    String speech;
+    public Speaker(String name, float satisfactionLevel, String speech) {
+        super(name, satisfactionLevel);
+        this.speech = speech;
+    }
+    void say(){
+
+    }
+}
+
+class Dog extends Speaker {
+    public Dog(String name, float satisfactionLevel, String speech) {
+        super(name, satisfactionLevel, speech);
+    }
+}
+
+class Cat extends Speaker {
+    public Cat(String name, float satisfactionLevel, String speech) {
+        super(name, satisfactionLevel, speech);
+    }
+}
+
+class Bird extends Dog {
+    public Bird(String name, float satisfactionLevel, String speech) {
+        super(name, satisfactionLevel, speech);
+    }
+}
+
+class Fish extends Animal {
+    public Fish(String name, float satisfactionLevel) {
+        super(name, satisfactionLevel);
     }
 }
